7b6120601da212721281cb48ebc198cf2fb27d00
Spirit-7 <Jotonamo@gmail.com>
2022-02-07 18:09:41 -0700

    activity 07

---
+++ Activity07/AdGenerator.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * AdGenerator.java
+ * 2/7/2022
+ * @author Joey Weston
+ * Uses Scanner, String, and NumberFormat classes to generate a service
+ * advertisement based on the inputed user's profile information.
+ */
+public class AdGenerator 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
