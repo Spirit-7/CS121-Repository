37ebf1fdf3c3481ca1224cafeadaa4f89604d65d
Spirit-7 <Jotonamo@gmail.com>
2022-02-07 19:34:24 -0700

    Finished

--- Activity07/AdGenerator.java
+++ Activity07/AdGenerator.java
@@ -1,4 +1,6 @@
-
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * AdGenerator.java
@@ -11,6 +13,59 @@ public class AdGenerator
 {
 	public static void main(String[] args)
 	{
+		// Create Scanner object
+		Scanner scnr = new Scanner(System.in);
 		
+		// Create Currency Format
+		NumberFormat currencyFmt = NumberFormat.getCurrencyInstance();
+		
+		// Welcome message
+		System.out.println("Welcome to AdGenerator");
+		System.out.println("Please enter your profile information...");
+		System.out.println("____________________________________________________________");
+		System.out.println("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+		System.out.println("____________________________________________________________");
+		
+		// Entered information variable storage
+		// First Name:
+		System.out.print("First Name: ");
+		String firstName = scnr.nextLine();
+		
+		// Middle Name:
+		System.out.print("Middle Name: ");
+		String middleName = scnr.nextLine();
+		
+		// Last Name:
+		System.out.print("Last Name: ");
+		String lastName = scnr.nextLine();
+		
+		// Job Title:
+		System.out.print("Job Title: ");
+		String jobTitle = scnr.nextLine();
+		
+		// Phone Number:
+		System.out.print("Phone Number (10 digit): ");
+		String phoneNum = scnr.nextLine();
+		
+		// Hourly Rate:
+		System.out.print("Hourly Rate: ");
+		double hourRate = scnr.nextDouble();
+		
+		// Divider
+		System.out.println("\n____________________________________________________________");
+		System.out.println("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+		System.out.println("____________________________________________________________");
+		
+		// Service Advertisement
+		
+		System.out.println("Need a " + jobTitle + "?");
+		System.out.println("CALL NOW!  (" + phoneNum.substring(0, 3) + ")  " + phoneNum.substring(3, 6) + "-" + phoneNum.substring(6));
+		System.out.println("Ask for " + firstName + " " +  middleName.charAt(0) + ". " + lastName);
+		System.out.println("(Rates start at " + currencyFmt.format(hourRate) + "/hr)");
+		
+		//Divider
+		System.out.println("____________________________________________________________");
+		System.out.println("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+		System.out.println("____________________________________________________________");
 	}
 }
