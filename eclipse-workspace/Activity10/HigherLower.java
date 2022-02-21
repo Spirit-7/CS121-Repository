6dc633196e2c4a3b5c59be64171c9dc07ebdcfe8
Spirit-7 <Jotonamo@gmail.com>
2022-02-20 17:42:17 -0700

    Merge branch 'main' of https://github.com/Spirit-7/CS121-Repository.git into main

--- eclipse-workspace/Activity10/HigherLower.java
+++ eclipse-workspace/Activity10/HigherLower.java
@@ -15,35 +15,62 @@ public class HigherLower
 		final int MAX = 10;
 		int answer;
 		int guess;
+		char yesOrNo;
+		boolean optToPlay = true;
 		
 		Scanner scan = new Scanner(System.in);
 		Random random = new Random();
 		
-		answer = random.nextInt(MAX) + 1;
+		while (optToPlay)
+		{
+			answer = random.nextInt(MAX) + 1;
 		
-		System.out.print("I'm thinking of a number between 1 and " + MAX + ". ");
-		System.out.print("Guess what it is: ");
+			System.out.print("I'm thinking of a number between 1 and " + MAX + ". ");
+			System.out.print("Guess what it is: ");
 		
-//		guess = scan.nextInt(); //Warning! Using nextInt() will screw up your ability
+			//		guess = scan.nextInt(); //Warning! Using nextInt() will screw up your ability
 					//to read anything other than ints later on!
-		guess = Integer.parseInt(scan.nextLine());	//THIS is how you should read
-								//a line of input when an int
-								//is expected and you still want
-								//to be able to read something
-								//else later.
-		
-		if(guess == answer)
-		{
+			guess = Integer.parseInt(scan.nextLine());	//THIS is how you should read
+														//a line of input when an int
+														//is expected and you still want
+														//to be able to read something
+														//else later.
+			while (guess != answer)
+			{
+				if ((guess > MAX) || (guess < 1))
+				{
+					System.out.println("Your guess is out of the range!");
+				}
+				else if (guess < answer)
+				{
+					System.out.println("Guess higher");
+				}
+				else
+				{
+					System.out.println("Guess lower");
+				}
+				guess = Integer.parseInt(scan.nextLine());
+			}
 			System.out.println("You got it! Good guessing!");
+			System.out.println("");
+			System.out.println("Would you like to play again? (y/n) ");
+			yesOrNo = scan.nextLine().charAt(0);
+			if (yesOrNo == 'y')
+			{
+				optToPlay = true;
+			}
+			else if (yesOrNo == 'n')
+			{
+				optToPlay = false;
+			}
+			else
+			{
+				System.out.println("Your input was invalid! Quitting game...");
+				optToPlay = false;
+			}
+			System.out.println("");
 		}
-		else
-		{
-			System.out.println("That is not correct, sorry.");
-			System.out.println("The number was " + answer + ".");
-		}
-		
 		System.out.println("Game over. Goodbye!");
-		
 		scan.close();
 	}
 }
